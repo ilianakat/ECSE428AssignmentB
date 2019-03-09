a9e00172ffaa5cea649a7cf19bc4532532decb16
Marine Huynh <marine.huynh@yahoo.ca>
2019-03-08 23:53:38 -0500

    Example of StepDefinitions.class

--- src/test/java/com/cucumber/StepDefinitions.java
+++ src/test/java/com/cucumber/StepDefinitions.java
@@ -1,4 +1,45 @@
 package com.cucumber;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class StepDefinitions {
+	
+	
+	@Given("I am logged into a Gmail account")
+	public void i_am_logged_into_a_Gmail_account() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I press ?New Message?")
+	public void i_press_New_Message() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I specify a recipient")
+	public void i_specify_a_recipient() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I select a file to attach")
+	public void i_select_a_file_to_attach() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I press ?send?")
+	public void i_press_send() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("the email should be sent.")
+	public void the_email_should_be_sent() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
 }
