f8fb864d134fb96100cb24a403ff8fe33757afe6
Marine Huynh <marine.huynh@yahoo.ca>
2019-03-10 15:11:55 -0400

    check initial state

--- src/test/java/com/cucumber/StepDefinitions.java
+++ src/test/java/com/cucumber/StepDefinitions.java
@@ -1,5 +1,8 @@
 package com.cucumber;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.net.MalformedURLException;
 
 import org.junit.Assert;
@@ -16,9 +19,9 @@ import cucumber.api.java.en.When;
 public class StepDefinitions {
 
 	private WebDriver driver;
-	
+
 	private final String PATH_TO_CHROME_DRIVER = System.getProperty("user.dir") + "/chromedriver.exe";
-	
+
 	private final String GMAIL_URL = "https://mail.google.com/mail/u/0/#inbox";
 
 	private final String IMAGEPATH = System.getProperty("user.dir") + "/attachments/";
@@ -36,9 +39,18 @@ public class StepDefinitions {
 	private final String CLASS_DRIVE = "Kj-JD-K7-K0";
 	private final String CLASS_SEND_THROUGH_DRIVE = "span";
 	
-	private final String test = "span.quantumWizButtonPaperbuttonLabel.exportLabel";
+
+	private final String test = "quantumWizButtonEl.quantumWizButtonPaperbuttonEl"
+			+ ".quantumWizButtonPaperbuttonFlat.quantumWizButtonPaperbuttonFlatColored"
+			+ ".quantumWizButtonPaperbutton2El2.quantumWizDialogPaperdialogDialogButton"
+			+ ".exportDefaultDialogButton.appsShareTeamdriveAclfixerButton.exportFocusable"
+			+ ".primaryButton";
 	
+	
+
 	private final String CLASS_SEND = "gU";
+	private final String CONFIRM = "aT";
+
 	@Given("^I am logged into a Gmail account as a user$")
 	public void i_am_logged_into_a_Gmail_account_as_a_user() {
 		try {
@@ -47,7 +59,7 @@ public class StepDefinitions {
 			System.out.println("Exception catched");
 		}
 		goTo(GMAIL_URL);
-
+		
 		driver.findElement(By.className(CLASS_EMAIL_PASSWORD)).sendKeys(EMAIL);
 		WebElement nextBTN = (new WebDriverWait(driver, 5))
 				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_NEXT)));
@@ -64,17 +76,23 @@ public class StepDefinitions {
 				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_NEXT)));
 		nextBTN.click();
 	}
-	
+
 	@Given("^I am writing a new message$")
 	public void i_am_writing_a_new_message() {
+		
+
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		//assertion called needs to be called before the new 
+		Assert.assertEquals(driver.getCurrentUrl().toString(), GMAIL_URL);
 		WebElement btn = (new WebDriverWait(driver, 10))
 				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_NEW_MESSAGE)));
 		btn.click();
+		
+
 	}
 
 	@Given("^the message is to \"([^\"]*)\"$")
@@ -84,7 +102,7 @@ public class StepDefinitions {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		driver.findElement(By.className(CLASS_MESSAGE_TO)).sendKeys(emailAddress);
 		driver.findElement(By.className(CLASS_SUBJECT)).sendKeys("Send with attachment");
 	}
@@ -96,29 +114,29 @@ public class StepDefinitions {
 	 */
 	@When("^I press on the button Attach File$")
 	public void i_press_on_the_button_Attach_File() {
-		
+
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		//file browser window but cannot close it after...
-		
-//		WebElement btn = (new WebDriverWait(driver, 10))
-//				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_ATTACHMENT)));
-//		btn.click();
+
+		//		WebElement btn = (new WebDriverWait(driver, 10))
+		//				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_ATTACHMENT)));
+		//		btn.click();
 	}
 
 	@When("^I select a file \"([^\"]*)\" from my file explorer$")
 	public void i_select_a_file_from_my_file_explorer(String file) {
-//		try {
-//			Thread.sleep(3000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
+		//		try {
+		//			Thread.sleep(3000);
+		//		} catch (InterruptedException e) {
+		//			e.printStackTrace();
+		//		}
 
 		//line after, going directly in the project's folder
-        driver.findElement(By.xpath(XPATH_ATTACHMENT)).sendKeys(IMAGEPATH+file);
+		driver.findElement(By.xpath(XPATH_ATTACHMENT)).sendKeys(IMAGEPATH+file);
 		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
@@ -126,7 +144,7 @@ public class StepDefinitions {
 		}
 		System.out.println("file has been uploaded");
 	}
-	
+
 	/*
 	 * Send the email 
 	 */
@@ -135,8 +153,20 @@ public class StepDefinitions {
 		WebElement btn = (new WebDriverWait(driver, 10))
 				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_SEND)));
 		btn.click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		WebElement sent = (new WebDriverWait(driver, 10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.className(CONFIRM)));
+
+		boolean ans = sent.getText().contains("Message sent.");
+		Assert.assertTrue(ans);
+		Assert.assertEquals(driver.getCurrentUrl().toString(), GMAIL_URL);
 	}
-	
+
 	/*
 	 * does not send the email
 	 */
@@ -155,7 +185,7 @@ public class StepDefinitions {
 	 * Case when it is larger than 25MB
 	 */
 	@Then("^the file will be included as a Google Drive file$")
-	public void the_file_will_be_included_as_a_Google_Drive_file() {
+	public void the_file_will_be_included_as_a_Google_Drive_file() throws AWTException {
 		try {
 			Thread.sleep(45000);
 		} catch (InterruptedException e) {
@@ -168,21 +198,34 @@ public class StepDefinitions {
 				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_SEND)));
 		btn.click();
 		System.out.println("debug: button for send reached");
-		
+
 		try {
 			Thread.sleep(3000);
 			System.out.println("debug: sleep 3000");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-//		btn = (new WebDriverWait(driver, 10))
-//				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_SEND_THROUGH_DRIVE)));
-//		btn.click();
-		
+		//		btn = (new WebDriverWait(driver, 10))
+		//				.until(ExpectedConditions.elementToBeClickable(By.className(CLASS_SEND_THROUGH_DRIVE)));
+		//		btn.click();
+
 		//Send message as a drive link
+
+//			Robot robot = new Robot();
+//			robot.keyPress(KeyEvent.VK_ENTER);
+
+		
 		btn = (new WebDriverWait(driver, 10))
-		.until(ExpectedConditions.elementToBeClickable(By.className(test)));
-btn.click();
+				.until(ExpectedConditions.elementToBeClickable(By.className(test)));
+		btn.click();
+
+			
+			WebElement sent = (new WebDriverWait(driver, 10))
+					.until(ExpectedConditions.presenceOfElementLocated(By.className(CONFIRM)));
+
+			boolean ans = sent.getText().contains("Message sent.");
+			Assert.assertTrue(ans);
+			Assert.assertEquals(driver.getCurrentUrl().toString(), GMAIL_URL);
 	}
 
 	//helper methods
